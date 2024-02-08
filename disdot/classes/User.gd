65349766fb4661878303f8761e719ece9aeeb674
Swarkin <102416174+Swarkin@users.noreply.github.com>
2024-02-08 16:27:49 +0100

    indent 2 to 4

--- disdot/classes/User.gd
+++ disdot/classes/User.gd
@@ -21,20 +21,20 @@ func _init(d: Dictionary) -> void:
 	public_flags = _safe_get(d, 'public_flags', 0) as int
 	avatar_decoration = _safe_get(d, 'avatar_decoration', '') as String
 
-var id: int										## the user's id
-var username: String					## the user's username, not unique across the platform
-var discriminator: int				## the user's Discord-tag
-var global_name: String				## the user's display name, if it is set. For bots, this is the application name
-var avatar: String						## the user's avatar hash
-var bot: bool									## whether the user belongs to an OAuth2 application
-var system: bool							## whether the user is an Official Discord System user (part of the urgent message system)
-var mfa_enabled: bool					## whether the user has two factor enabled on their account
-var banner: String						## the user's banner hash
-var accent_color: int					## the user's banner color encoded as an integer representation of hexadecimal color code
-var locale: String						## the user's chosen language option
-var verified: bool						## whether the email on this account has been verified
-var email: String							## the user's email
-var flags: int								## the flags on a user's account
-var premium_type: int					## the type of Nitro subscription on a user's account
-var public_flags: int					## the public flags on a user's account
+var id: int						## the user's id
+var username: String			## the user's username, not unique across the platform
+var discriminator: int			## the user's Discord-tag
+var global_name: String			## the user's display name, if it is set. For bots, this is the application name
+var avatar: String				## the user's avatar hash
+var bot: bool					## whether the user belongs to an OAuth2 application
+var system: bool				## whether the user is an Official Discord System user (part of the urgent message system)
+var mfa_enabled: bool			## whether the user has two factor enabled on their account
+var banner: String				## the user's banner hash
+var accent_color: int			## the user's banner color encoded as an integer representation of hexadecimal color code
+var locale: String				## the user's chosen language option
+var verified: bool				## whether the email on this account has been verified
+var email: String				## the user's email
+var flags: int					## the flags on a user's account
+var premium_type: int			## the type of Nitro subscription on a user's account
+var public_flags: int			## the public flags on a user's account
 var avatar_decoration: String	## the user's avatar decoration hash
