fda70e4d183fc7a2a7a3ce4ce17440e0f6a654ef
Swarkin <102416174+Swarkin@users.noreply.github.com>
2024-03-09 15:03:54 +0100

    Update BetterWebsocket.gd

--- disdot/deps/BetterWebsocket.gd
+++ disdot/deps/BetterWebsocket.gd
@@ -1,7 +1,7 @@
-@icon('res://classes/BetterWebsocket.svg')
+@icon('../BetterWebsocket.svg')
 class_name BetterWebsocket
-extends Node
-## BetterWebsocket v1.0.2 by swark1n
+extends Node  # Can also work as a non-Node type
+## BetterWebsocket v1.1.0 by swark1n
 
 signal connected
 signal disconnected
@@ -11,9 +11,9 @@ signal packet_received(content: PackedByteArray)
 
 @export var verbose := false
 
-var s := WebSocketPeer.new()				## The underlying Websocket.
+var s := WebSocketPeer.new()		## The underlying Websocket.
 var _processing := false
-var _prev_state := s.STATE_CLOSED		## To get the Websocket state, use [code]better_websocket.s.get_ready_state()[/code] instead.
+var _prev_state := s.STATE_CLOSED	## To get the Websocket state, use [code]better_websocket.s.get_ready_state()[/code] instead.
 
 
 func _process(_dt: float) -> void:
@@ -36,27 +36,29 @@ func _process(_dt: float) -> void:
 				packet_received.emit(s.get_packet())
 
 		s.STATE_CLOSED:
-			disconnected.emit()
 			_processing = false
+			disconnected.emit()
+			if verbose: print('BetterWebsocket | Connection closed')
 
 
 func begin_connection(url: String) -> Error:
 	var err := s.connect_to_url(url)
+	if err:
+		var close_reason := s.get_close_reason()
+		var close_code := s.get_close_code()
+		connection_failed.emit(close_code, close_reason)
+		if verbose: push_error('BetterWebsocket | Connection failed: ', error_string(err), ', code: ', close_code, ', reason: ', close_reason)
 
-	if not err:
-		_processing = true
-		return err  # OK
+		return err
 
-	var close_reason := s.get_close_reason()
-	var close_code := s.get_close_code()
-	connection_failed.emit(close_code, close_reason)
-	if verbose:
-		push_error('BetterWebsocket | Connection failed: ', error_string(err), ', code: ', close_code, ', reason: ', close_reason)
+	_processing = true
+	return err  # OK
 
-	return err
 
 func close_connection(code := 1000) -> void:
 	s.close(code)
+	if verbose: print('BetterWebsocket | Closing...')
+
 
 func send_packet(content: String) -> Error:
 	return s.put_packet(content.to_utf8_buffer())
